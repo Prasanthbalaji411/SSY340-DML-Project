9d233b7fff601c6aa1358e8d99fae0e8dcd8fac0
Prasanth411 <prasanthbalaji411.com>
2024-10-25 12:53:01 +0200

    Deleted the large unneccessary file

--- main_oct16.py
+++
@@ -1,8 +0,0 @@
-from Preprocessing import *
-from GAN import *
-from CarClassifier import *
-from evaluation import *
-
-train_dataset = datasets.ImageFolder(root=destination_path+'/train', transform=transform)
-test_dataset = datasets.ImageFolder(root=destination_path+'/test', transform=transform)
-
